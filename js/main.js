587df96a2ba634c1c42ded90ba14b05560aa3320
Brenden <brenden.niedermeyer@gmail.com>
2016-11-17 19:09:33 -0800

    formatting updates

--- js/main.js
+++ js/main.js
@@ -1,34 +1,37 @@
-var tips = [
-  {"text": "",
-  "imageUrl": null}
-];
+var tips = [{
+    "text": "",
+    "imageUrl": null
+}];
 
-function addTip(newTip) {
-  tips.push(newTip);
+function addTip(newTipText) {
+    var newTip = {
+        "text": newTipText,
+        "imageUrl": null
+    };
+    tips.push(newTip);
 }
 
 function getRandomTip() {
-  var min = Math.ceil(0);
-  var max = Math.floor(tips.length);
-  var randomIndex = Math.floor(Math.random() * (max - min)) + min;
-  return tips[randomIndex];
+    var min = Math.ceil(0);
+    var max = Math.floor(tips.length);
+    var randomIndex = Math.floor(Math.random() * (max - min)) + min;
+    return tips[randomIndex];
 }
 
 $(document).ready(function() {
-  $('.submit-tip').click(function() {
-    addTip($('.input-tip').val());
-  });
+    $('.submit-tip').click(function() {
+        addTip($('.input-tip').val());
+    });
 
-  $('.get-tip').click(function() {
-    var tip = getRandomTip();
-    $('.tip-container').value(tip.text);
-    if (tip.imageUrl === null) {
-      $('.tip-image').hide();
-    } else {
-      $('.tip-image').attr("src", tip.imageUrl);
-      $('.tip-image').show();
-
-    }
-  });
+    $('.get-tip').click(function() {
+        var tip = getRandomTip();
+        $('.tip-container').value(tip.text);
+        if (tip.imageUrl === null) {
+            $('.tip-image').hide();
+        } else {
+            $('.tip-image').attr("src", tip.imageUrl);
+            $('.tip-image').show();
+        }
+    });
 
 });
