8b7bfe2bc0b5d8ec66af551f79c86aac68279b56
Brenden <brenden.niedermeyer@gmail.com>
2016-11-17 19:23:38 -0800

    Added some default tips

--- js/main.js
+++ js/main.js
@@ -1,6 +1,18 @@
 var tips = [{
-    "text": "",
-    "imageUrl": null
+    "text": "Add your favorite booze to the eggnog to make your Uncle Bob's vacation stories that much more entertaining.",
+    "imageUrl": "https://harriedhousewifeblog.files.wordpress.com/2012/12/spiked-eggnog-punch1.jpg"
+}, {
+    "text": "Don't bring up politics at dinner!",
+    "imageUrl": "http://www.rawstory.com/wp-content/uploads/2015/11/SNL-thanksgiving-800x430.jpg"
+}, {
+    "text": "Don't bring up politics at dinner!",
+    "imageUrl": "http://www.rawstory.com/wp-content/uploads/2015/11/SNL-thanksgiving-800x430.jpg"
+}, {
+    "text": "That can of cranberry sauce will help you break the window to your car when you lock you keys in it while out to grab 'one last thin' before dinner",
+    "imageUrl": "http://blogs.wickedlocal.com/massmarkets/files/2011/12/acransauce2.jpg"
+}, {
+    "text": "Choose different giftwrap for each person so you can easily tell who gets which present.",
+    "imageUrl": "https://thumbs.dreamstime.com/x/pile-christmas-gift-12117042.jpg"
 }];
 
 function addTip(newTipText) {
@@ -33,5 +45,4 @@ $(document).ready(function() {
             $('.tip-image').show();
         }
     });
-
 });
